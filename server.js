1af7d33daa197305d2f87fb98c25338ce90afe42
zee <zperezcanals@gmail.com>
2018-08-15 16:59:47 -0400

    app setup backend

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-console.log('hello world');
+// console.log('hello world');
 
 const app = express();
 
+// main page
+
 app.get('/', (req, res) => {
-	console.log('I am home');
+	res.send('I am home');
+
 })
 
-app.post('/signin', (req,res)=>{
-	console.log('i am in signin')
+// member log-in
+app.get('/signin', (req,res)=>{
+	res.send('i am in signin');
 })
 
-app.post('/signup', (req, res)=>{
-	console.log('i am in signup with spotify')
+//new member sign-up
+app.get('/signup', (req, res)=>{
+	res.send('i am in signup with spotify');
 })
 
+//member profile
 app.get('/profile/:id', (req,res)=>{
-	console.log('I am in profile')
+	res.send('I am in profile');
 })
+
 
 app.listen(3000, ()=> {
   console.log('app is running on port 3000');
