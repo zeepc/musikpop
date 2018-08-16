3db3f193ff5a49b905ea6ed3804fae41bb2f50e6
zee <zperezcanals@gmail.com>
2018-08-16 11:30:28 -0400

    initial passport setup

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const SpotifyStrategy = require('passport-spotify').Strategy;
 
-// console.log('hello world');
+
+
 
 const app = express();
 
 passport.use(
   new SpotifyStrategy(
     {
-      clientID: client_id,
-      clientSecret: client_secret,
+      clientID: process.env.SPOTIFY_ID,
+      clientSecret: process.env.SPOTIFY_SECRET,
       callbackURL: 'http://localhost:8888/auth/spotify/callback'
     },
     function(accessToken, refreshToken, expires_in, profile, done) {
@@ -21,6 +23,7 @@ passport.use(
   )
 );
 
+
 // main page
 
 app.get('/', (req, res) => {
@@ -29,21 +32,21 @@ app.get('/', (req, res) => {
 })
 
 // member log-in
-app.post('/signin', (req,res)=>{
+app.post('/signin', (req,res) => {
 	res.send('i am in signin');
 })
 
 //new member sign-up
-app.get('/signup', (req, res)=>{
+app.get('/signup', (req, res) => {
 	res.send('i am in signup with spotify');
 })
 
 //member profile
-app.get('/profile/:id', (req,res)=>{
+app.get('/profile/:id', (req,res) => {
 	res.send('I am in profile');
 })
 
 
-app.listen(3000, ()=> {
+app.listen(3000, () => {
   console.log('app is running on port 3000');
 })
